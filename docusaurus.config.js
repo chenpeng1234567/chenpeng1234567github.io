a089b4e4c4c5320e65f1539fe1a362461e2acb81
chenpeng1234567 <119844588+chenpeng1234567@users.noreply.github.com>
2024-06-21 14:50:28 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,22 @@ const config = {
     locales: ['zh-CN'],
   },
 
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      },
+    ],
+  ],
+
   presets: [
     [
       'classic',
